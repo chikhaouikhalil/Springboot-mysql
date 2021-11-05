93ea6ee8b6a51c99bda6596a50a244288b3cb11c
chikhaouikhalil <79362891+chikhaouikhalil@users.noreply.github.com>
2021-11-05 16:45:38 +0100

    Connect MYSQL - Departement Entity
    CRUD OPERATION

---
+++ mysqlbackendjava/src/main/java/com/mysqlbackend/mysqlbackendjava/MysqlbackendjavaApplication.java
@@ -0,0 +1,13 @@
+package com.mysqlbackend.mysqlbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlbackendjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlbackendjavaApplication.class, args);
+	}
+
+}
---
+++ mysqlbackendjava/src/test/java/com/mysqlbackend/mysqlbackendjava/MysqlbackendjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysqlbackend.mysqlbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlbackendjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
